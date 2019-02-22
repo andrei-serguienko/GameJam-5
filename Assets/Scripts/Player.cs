f8f8457c8120d5a089edd2066e777ce2b518fc82
Andrei Serguienko <andrei.serguienko@etu.univ-amu.fr>
2019-02-22 22:32:39 +0000

    1

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+   
+    public float speed;
+    public float timeBetweenAttack;
+    private bool jumping = false;
+    private bool attack = false;
+    private bool facingRight = true;
+
+    public int forceJump;
+    
+    void Flip(string arg)
+    {
+        if (arg == "left")
+        {
+            if (facingRight)
+            {
+                Vector3 theScale = transform.localScale;
+                theScale.x *= -1;
+                transform.localScale = theScale;
+                facingRight = false;
+            }
+        } else if (arg == "right")
+        {
+            if (!facingRight)
+            {
+                Vector3 theScale = transform.localScale;
+                theScale.x *= -1;
+                transform.localScale = theScale;
+                facingRight = true;
+            }
+        }
+    }
+    
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+
+        Vector3 movement = new Vector3 (moveHorizontal * speed, 0, 0);
+
+        transform.position += movement * Time.deltaTime;
+
+        if (movement.x < 0)
+        {
+            Flip("left");
+        }
+        else if (movement.x > 0)
+        {
+            Flip("right");
+        }
+    }
+    
+    void Update () {
+        if (Input.GetKeyDown("space") && jumping == false)
+        {
+            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, forceJump);
+            jumping = true;
+        }
+
+        print(attack);
+        if (Input.GetMouseButtonDown(0) && attack == false)
+        {
+            attack = true;
+            transform.GetChild(0).gameObject.SetActive(true);
+            transform.GetChild(0).GetComponent<Attack>().cast();
+            Invoke("EnableAttack", timeBetweenAttack);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            jumping = false;
+        }
+        else
+        {
+            jumping = true;
+        }
+    }
+
+    void EnableAttack()
+    {
+        attack = false;
+    }
+}
---
+++ Assets/Scripts/Twin.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Twin : MonoBehaviour
+{
+    public GameObject player;
+    public float timeTwin;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Time.time > timeTwin)
+            transform.position = player.GetComponent<Follow>().positionQueue.Dequeue();
+
+        Facing();
+    }
+
+    private void Facing()
+    {
+            
+    }
+}
