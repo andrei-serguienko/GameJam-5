27a49c54aefe57b04659cdfa9dc1aaa39920bf2f
Andrei Serguienko <andrei.serguienko@etu.univ-amu.fr>
2019-02-23 02:13:30 +0000

    attack

--- Assets/Scripts/Attack.cs
+++ Assets/Scripts/Attack.cs
@@ -26,7 +26,7 @@ public class Attack : MonoBehaviour
         gameObject.SetActive(false);
     }
 
-    private void OnCollisionEnter2D(Collision2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         print(other);
         if (other.gameObject.tag == "Enemy")
