28be50af3bcb2d50ce20937c6e9af279c31aa141
Andrei Serguienko <andrei.serguienko@etu.univ-amu.fr>
2019-02-23 12:19:08 +0000

    Bee animation

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,31 +5,50 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     private GameObject player;
+    private Animator anim;
 
     public int enemySpeed;
 
-    public int health = 100;
+    public int health = 200;
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
+        Invoke("Attack", 3f);
         player = GameObject.FindGameObjectWithTag("Player");
+              
+    }
+
+    void Attack()
+    {
         
         Vector3 vectorToTarget = player.transform.position - transform.position;
         float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
         Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
         transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * 100);
         
-        gameObject.GetComponent<Rigidbody2D>().velocity = gameObject.transform.right * enemySpeed;        
+        gameObject.GetComponent<Rigidbody2D>().velocity = gameObject.transform.right * enemySpeed;  
     }
-
     // Update is called once per frame
     void Update()
     {
         checkAlive();
+
+        if (Mathf.Abs(gameObject.GetComponent<Rigidbody2D>().velocity.x) > 0 || Mathf.Abs(gameObject.GetComponent<Rigidbody2D>().velocity.y) > 0)
+        {
+            anim.SetBool("Moving", true);
+        }
+        else
+        {
+            anim.SetBool("Moving", false);
+        }
     }
 
     public void takeDamage(int dmg)
     {
+        gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        Invoke("Attack", 2f);
+        anim.SetTrigger("Hit");
         health -= dmg;
     }
 
